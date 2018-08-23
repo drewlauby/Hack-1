648d8c5eb410d26776082b674b2389292aa0758f
drewlauby <42359145+drewlauby@users.noreply.github.com>
2018-08-23 16:29:14 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Daniel Cornelius
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Daniel Cornelius, Electric Engineering, Jarrod Blaser, Mechanical Engineering, Drew Lauby, Computer Engineering\n");
+
+  return 0;
+}
